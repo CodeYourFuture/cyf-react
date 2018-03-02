1bb7f750e8c5c3c1b6d3aef1704770c9f839f4cd
Matt Paul <mattpaul001@icloud.com>
2018-03-02 17:39:07 +0200

    Mock 'withRouter', update snapshots

--- src/pages/home/index.test.js
+++ src/pages/home/index.test.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './';
 
-jest.mock('react-router-dom', () => ({
-  Link: () => <div />,
-}));
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Home />, div);
