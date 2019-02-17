a4055dd5c19b92d4a76312eafbbbf0ef12ab8051
Jonathan Schmid <hi@jonathanschmid.de>
2019-02-17 20:38:21 +0000

    Add non-technical skills to What We Teach

--- src/components/what-we-teach/index.js
+++ src/components/what-we-teach/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import honeyCombIcon from '../../assets/images/icons/honeycomb.svg';
 import tabletIcon from '../../assets/images/icons/tablet.svg';
 import codeIcon from '../../assets/images/icons/code.svg';
 import nodeJsIcon from '../../assets/images/icons/nodejs.svg';
@@ -17,7 +18,17 @@ const WhatWeTeach = () => (
         </div>
       </div>
       <div className="row">
-        <div className="col-sm-3">
+        <div className="col-sm-5ths">
+          <div className="md-icon">
+            <img src={honeyCombIcon} alt="Honeycomb" />
+          </div>
+          <h4>Non-technical skills</h4>
+          <p>
+            Vital techniques and tools to make learning and working together
+            easier and more effective.
+          </p>
+        </div>
+        <div className="col-sm-5ths">
           <div className="md-icon">
             <img src={tabletIcon} alt="Tablet" />
           </div>
@@ -27,7 +38,7 @@ const WhatWeTeach = () => (
             practices.
           </p>
         </div>
-        <div className="col-sm-3">
+        <div className="col-sm-5ths">
           <div className="md-icon">
             <img src={codeIcon} alt="Code" />
           </div>
@@ -36,14 +47,14 @@ const WhatWeTeach = () => (
             Use powerful modern scripting language to build web applications.
           </p>
         </div>
-        <div className="col-sm-3">
+        <div className="col-sm-5ths">
           <div className="md-icon">
             <img src={nodeJsIcon} alt="NodeJS" />
           </div>
           <h4>Node.js</h4>
           <p>Web application development powered by modern and fast Node.js</p>
         </div>
-        <div className="col-sm-3">
+        <div className="col-sm-5ths">
           <div className="md-icon">
             <img src={reactIcon} alt="React" />
           </div>
